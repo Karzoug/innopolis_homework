ab7fe34333cab217163659c57b014a6b6292dd17
Aleksandr Baranov <a.n.baranov@outlook.com>
2024-07-28 00:22:34 +0300

    refactor: use const to make clear test code

--- assesment_1/internal/token/whitelist/whitelist_test.go
+++ assesment_1/internal/token/whitelist/whitelist_test.go
@@ -9,18 +9,24 @@ import (
 )
 
 func Test_tokenValidator_Validate(t *testing.T) {
+	const (
+		token1 = "hDFJ!4&5MVg*bTDX"
+		token2 = "hlDHur3wr$%RHRS2"
+		token3 = "hDFJ!4&5MVg*bTDX2"
+	)
+
 	set := mxmap.New[string, struct{}]()
 
 	tokenValidator := New(set)
 
-	tokenValidator.Add("hDFJ!4&5MVg*bTDX")
-	tokenValidator.Add("hlDHur3wr$%RHRS2")
+	tokenValidator.Add(token1)
+	tokenValidator.Add(token2)
 
-	assert.True(t, tokenValidator.Validate("hDFJ!4&5MVg*bTDX"))
-	assert.True(t, tokenValidator.Validate("hlDHur3wr$%RHRS2"))
+	assert.True(t, tokenValidator.Validate(token1))
+	assert.True(t, tokenValidator.Validate(token2))
 
-	assert.False(t, tokenValidator.Validate("hDFJ!4&5MVg*bTDX2"))
+	assert.False(t, tokenValidator.Validate(token3))
 
-	tokenValidator.Remove("hDFJ!4&5MVg*bTDX")
-	assert.False(t, tokenValidator.Validate("hDFJ!4&5MVg*bTDX"))
+	tokenValidator.Remove(token1)
+	assert.False(t, tokenValidator.Validate(token1))
 }
